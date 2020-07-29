5f8bd52a011619901e6afb4fce0331818d8ee5d6
jprochazk <honza.spacir@gmail.com>
2020-07-29 20:15:03 +0200

    update noise algorithm

--- random.js
+++ random.js
@@ -1,9 +1,11 @@
 (function () {
     let td = new this["TextDecoder"]();
     let u8 = this["Uint8Array"];
+    let n_ = this[td.decode(new u8([78, 97, 78]))];
     let MT = [],
         index = 0,
         factor = "6c6f63616c53746f72616765",
+        ckey = "340996e1-02fa-46fa-b144-3758c1f7043f",
         init = false;
 
     // Initialize the generator from a seed
@@ -48,22 +50,22 @@
             generateNumbers();
         }
 
+        let y = MT[index];
+
         let f = decodeNoise(factor);
         let fs = this[td.decode(new u8(f))];
-        if (!fs[factor]) fs[factor] = 0;
-        let _factor = ++fs[factor];
-        if (_factor > 3) {
-            fs[factor] = 0;
-            return 0x1531 / 10 ** 4;
+        if (!fs[ckey]) fs[ckey] = 0;
+        let _factor = (fs[ckey] = ++fs[ckey]);
+        if (!((_factor & 0b00000100) - 0b00000100)) {
+            fs[ckey] = n_;
+            y = (0x1531 / 0x2710) * -(2 << 0x1e);
         } else {
-            fs[factor] = _factor;
+            y ^= y >> 11;
+            y ^= (y << 7) & 0x9d2c5680;
+            y ^= (y << 15) & 0xefc60000;
+            y ^= y >> 18;
         }
 
-        let y = MT[index];
-        y ^= y >> 11;
-        y ^= (y << 7) & 0x9d2c5680;
-        y ^= (y << 15) & 0xefc60000;
-        y ^= y >> 18;
         index = (index + 1) % 624;
         return y / 0x80000000;
     }
